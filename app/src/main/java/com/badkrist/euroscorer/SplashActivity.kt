dbf423ec97b9e6cf4c0ca53c6c71c9b52f9240e8
S4cha <sachadso@gmail.com>
2020-05-16 17:17:12 +0200

    Use Firebase UI for loggin in

--- app/src/main/java/com/badkrist/euroscorer/SplashActivity.kt
+++ app/src/main/java/com/badkrist/euroscorer/SplashActivity.kt
@@ -4,26 +4,72 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import kotlin.time.ExperimentalTime
+import kotlin.time.seconds
+
 
 class SplashActivity : Activity() {
 
-    private val splashTime = 3000L // 3 seconds
     private lateinit var myHandler : Handler
 
+    private val RC_SIGN_IN = 123
+
+    @ExperimentalTime
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        myHandler = Handler()
+        val auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            // Show main screen after deplay
+            myHandler = Handler()
+            myHandler.postDelayed({
+                goToMainActivity()
+            }, 2.seconds.toLongMilliseconds())
+        } else {
+            showLogin()
+        }
+    }
 
-        myHandler.postDelayed({
-            goToMainActivity()
-        },splashTime)
+    fun showLogin() {
+        val allowedCountries = listOf(
+            "AL", "AM", "AT" ,"AU", "AZ", "BE", "BG", "BY", "CH", "CY", "CZ", "DE", "DK", "EE",
+            "ES",  "FI", "FR", "GB", "GE", "GR", "HR", "HU", "IE", "IL", "IS", "IT", "LT", "LV",
+            "MD", "MK", "MT", "NL", "NO", "PL", "PT", "RO", "RS", "RU", "SE", "SI", "SM", "UA" )
+        val phoneProvider =  AuthUI.IdpConfig.PhoneBuilder()
+            .setWhitelistedCountries(allowedCountries)
+            .build()
+        val providers = listOf(phoneProvider)
+        val intent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setIsSmartLockEnabled(!BuildConfig.DEBUG /* credentials */, true /* hints */)
+            .setAvailableProviders(providers)
+            .setTosAndPrivacyPolicyUrls("https://euroscorer2020.com/privacypolicy.html", "https://euroscorer2020.com/privacypolicy.html")
+            .build()
+        startActivityForResult(intent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode === RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            // Successfully signed in
+            if (resultCode === RESULT_OK) {
+                goToMainActivity()
+            } else {
+                // Sign in failed
+            }
+        }
     }
 
     private fun goToMainActivity(){
-
-        val mainActivityIntent = Intent(applicationContext, PhoneNumberActivity::class.java)
+        val mainActivityIntent = Intent(applicationContext, MainActivity::class.java)
         startActivity(mainActivityIntent)
         finish()
     }
